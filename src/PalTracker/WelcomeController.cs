9c26867bf06a7ea2451eb1d8c7ca1178d40e4ab9
Janson Joseph Chacko <jansonjc@gmail.com>
2020-04-26 22:12:15 +0530

    Simple .NET Core Web API app

---
+++ src/PalTracker/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalTracker
+{
+  [Route("/")]
+  public class WelcomeController : ControllerBase
+  {
+    [HttpGet]
+    public string SayHello() => "hello";
+  }
+}
