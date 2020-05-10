c3cad2073f844fbb6dfc69d3a9395dd246f34e1e
Janson Joseph Chacko <jansonjc@gmail.com>
2020-05-10 14:25:28 +0530

    debugging migrate-database.sh

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -11,6 +11,13 @@ db_name=`echo $credentials | jq -r '.name'`
 db_username=`echo $credentials | jq -r '.username'`
 db_password=`echo $credentials | jq -r '.password'`
 
+echo "app_guid=$app_guid"
+echo "credentials=$credentials"
+echo "ip_address=$ip_address"
+echo "db_name=$db_name"
+echo "db_username=$db_username"
+echo "db_password=$db_password"
+
 echo "Opening ssh tunnel to $ip_address"
 cf ssh -N -L 63306:$ip_address:3306 $1 &
 cf_ssh_pid=$!
